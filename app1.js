df275314fc99d3e69d925282e90e7c3793d4ceee
iker ena <p.gasol.16@hotmail.com>
2017-03-30 12:16:33 +0200

    practica 4

--- app1.js
+++ app1.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 
-var port =process.env.PORT || 3000;
+var port =process.env.PORT || 8080;
 app.set('view engine','jade');
 app.use(express.static('public'));
 var server = require('http').createServer(app);
